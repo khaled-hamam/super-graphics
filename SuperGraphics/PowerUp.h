4b3c524a4b94048abd379646fe67e2f401af4e99
mariamkamel <mariammkamel98@gmail.com>
2018-12-11 17:27:31 +0200

    nullptr  is pushed instead  of ResourceManager null pointer

--- SuperGraphics/PowerUp.h
+++ SuperGraphics/PowerUp.h
@@ -6,9 +6,9 @@ class PowerUp : public Model {
 public:
 	PowerUp() {
 		primitives = {
-			new Pyramid(ResourceManager::getTexture(""),vec3(2.0f, 0.65f, 0.f),vec3(0.f, 0.f, 0.f), vec3(0.2f),vec3(GREEN)),
-			new Pyramid(ResourceManager::getTexture(""),vec3(1.9f, 0.75f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.05f),vec3(GREEN)),
-			new Pyramid(ResourceManager::getTexture(""),vec3(2.1f, 0.75f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.05f),vec3(GREEN)),
+			new Pyramid(nullptr,vec3(2.0f, 0.65f, 0.f),vec3(0.f, 0.f, 0.f), vec3(0.2f),vec3(GREEN)),
+			new Pyramid(nullptr,vec3(1.9f, 0.75f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.05f),vec3(GREEN)),
+			new Pyramid(nullptr,vec3(2.1f, 0.75f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.05f),vec3(GREEN)),
 
 		};
 	}
--- SuperGraphics/Spike.h
+++ SuperGraphics/Spike.h
@@ -10,7 +10,7 @@ public:
 
 		GLfloat x = 4;
 		while (x <= 5) {
-			primitives.push_back(new Pyramid(ResourceManager::getTexture(""), vec3(x, 0.55f, 0.f), vec3(0.f, 0.f, 0.f), vec3(0.2f), vec3(BLUE)));
+			primitives.push_back(new Pyramid(nullptr, vec3(x, 0.55f, 0.f), vec3(0.f, 0.f, 0.f), vec3(0.2f), vec3(BLUE)));
 			x+=0.1f;
 		}
 	}
