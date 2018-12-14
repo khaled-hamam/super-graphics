363ed00c6ffefba5b10688e8d949a57c1e3156bf
mariamkamel <mariammkamel98@gmail.com>
2018-12-14 16:09:35 +0200

    handle keys input in hero added

--- SuperGraphics/Hero.h
+++ SuperGraphics/Hero.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Primitives.h"
+#include "directions.h"
 #include "Model.h"
 class Hero : public Model
 {
 public:
-	char direction=' ';
-	vec3 currentPos;
+	int direction;
+	vec3 lastPos;
+	float jumpStep = 0.01f, moveStep = 0.03f;
 	Hero() {
 		primitives = {
 			new Quad(ResourceManager::getTexture("hero"), vec3(-1.f, 1.f, 0.f), vec3(0.f), vec3(1.2f)),
@@ -13,38 +15,45 @@ public:
 
 	}
 	void update() {
-		if (direction == 'u') {
-			primitives[0]->position += vec3(0.f, 0.01f, 0.f);
-			cout << currentPos.y << ' '<<primitives[0]->position.y << ' '<<"UP"<<endl;
-			if ( primitives[0]->position.y- currentPos.y > 0.3f) {
-				direction = 'd';
+		if (direction == (int)Up) {
+			primitives[0]->position += vec3(0.f, jumpStep, 0.f);
+			if ( primitives[0]->position.y- lastPos.y > 100*jumpStep) {
+				direction = (int)Down;
 			}
 		}
-		else if (direction == 'd') {
-			primitives[0]->position -= vec3(0.f, 0.01f, 0.f);
-			cout << currentPos.y << ' ' << primitives[0]->position.y << ' ' << "down" << endl;
-			if ( primitives[0]->position.y- currentPos.y  == 0.f) {
+		else if (direction == (int)Down) {
+			primitives[0]->position -= vec3(0.f, jumpStep, 0.f);
+			if ( primitives[0]->position.y - lastPos.y  == 0.f) {
 				direction = ' ';
 			}
 		}
-		else if (direction == 'l') {
-			primitives[0]->position -= vec3(0.01f, 0.f, 0.f);
-			cout << currentPos.x << ' ' << primitives[0]->position.x << ' ' << "left" << endl;
-				direction = ' ';
-			
+		else if (direction == (int)Left) {
+			primitives[0]->position -= vec3(moveStep, 0.f, 0.f);
+			direction = ' ';			
 		}
-		else if (direction == 'r') {
-			primitives[0]->position += vec3(0.01f, 0.f, 0.f);
-			cout << currentPos.x << ' ' << primitives[0]->position.x << ' ' << "right" << endl;
-				direction = ' ';
-			
+		else if (direction == (int)Right) {
+			primitives[0]->position += vec3(moveStep, 0.f, 0.f);
+			direction = ' ';
 		}
 	}
-	void setDir( char dir) {
-		cout << "here" << endl;
-		if (dir != direction)
-			currentPos = primitives[0]->position;
+
+	void setDir(int  dir) {
+		if (dir != direction) {
+			lastPos = primitives[0]->position;
+		}
 		direction = dir;
+	}
+
+	void handelInput(GLFWwindow *Window) {
+		if (glfwGetKey(Window, GLFW_KEY_I) == GLFW_PRESS) {
+			this->setDir((int)Up);
+		}
+		if (glfwGetKey(Window, GLFW_KEY_J) == GLFW_PRESS) {
+			this->setDir((int)Left);
+		}
+		if (glfwGetKey(Window, GLFW_KEY_L) == GLFW_PRESS) {
+			this->setDir((int)Right);
+		}	
 	}
 	~Hero();
 };
