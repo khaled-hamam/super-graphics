ea39336107c01182a03713d11c4478e2f1b682bf
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-12-26 09:22:29 +0200

    Fixed Collision Bug for small models

--- SuperGraphics/Coin.h
+++ SuperGraphics/Coin.h
@@ -4,17 +4,19 @@
 
 class Coin : public Model {
 public:
-	Coin(vec3 position = vec3(0.f), vec3 rotaion = vec3(0.f), vec3 scale = vec3(1.f)) {
+	Coin(vec3 position = vec3(0.f), vec3 rotaion = vec3(0.f), vec3 scale = vec3(0.3f)) {
 		primitives = {
-			new Quad(ResourceManager::getTexture("coin"), vec3(0.f, 0.f, 0.f), vec3(0.f), vec3(0.3f)),
+			new Quad(ResourceManager::getTexture("coin"), vec3(0.f, 0.f, 0.f), vec3(0.f), vec3(1.f)),
 		};
 		move(position);
 		rotate(rotaion);
 		changeScale(scale);
 	}
+
 	void update() {
 		primitives[0]->rotation += vec3(0.f, 1.f, 0.f);
 	}
+
 	virtual void collision(Model *model, Directions directions, float distance) override {
 		Hero *hero = (Hero*)model;
 		this->destroyed = true;
--- SuperGraphics/PowerUp.h
+++ SuperGraphics/PowerUp.h
@@ -4,16 +4,18 @@
 
 class PowerUp : public Model {
 public:
-	PowerUp(vec3 position = vec3(0.f), vec3 rotaion = vec3(0.f), vec3 scale = vec3(1.f)) {
+	PowerUp(vec3 position = vec3(0.f), vec3 rotaion = vec3(0.f), vec3 scale = vec3(0.2f)) {
 		primitives = {
-			new Pyramid(nullptr,vec3(0.f, 0.f, 0.f),vec3(0.f, 0.f, 0.f), vec3(0.2f),vec3(GREEN)),
-			new Pyramid(nullptr,vec3(-0.1f, 0.1f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.05f),vec3(GREEN)),
-			new Pyramid(nullptr,vec3(0.1f, 0.1f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.05f),vec3(GREEN))
+			new Pyramid(nullptr,vec3(0.f, 0.f, 0.f),vec3(0.f, 0.f, 0.f), vec3(1.f),vec3(GREEN)),
+			new Pyramid(nullptr,vec3(-0.1f, 0.1f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.25f),vec3(GREEN)),
+			new Pyramid(nullptr,vec3(0.1f, 0.1f, 0.f),vec3(0.f, 0.f, 180.f), vec3(0.25f),vec3(GREEN))
 		};
+
 		move(position);
 		rotate(rotaion);
 		changeScale(scale);
 	}
+
 	void update() {
 		primitives[0]->rotation += vec3(0.f, 1.0f, 0.f);
 		primitives[1]->rotation += vec3(0.f, -1.0f, 0.f);
